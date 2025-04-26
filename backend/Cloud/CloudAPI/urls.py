1c01aaaf210f34381053d05a7eedfb9ef9138eeb
huyapi2707 <dangdinhhuyisme@gmail.com>
2025-04-26 19:42:25 +0700

    #

--- backend/Cloud/CloudAPI/urls.py
+++ backend/Cloud/CloudAPI/urls.py
@@ -6,7 +6,7 @@ from CloudAPI.views import UserViewSet, ProxmoxViewSet
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet, basename='users')
-router.register('proxmox', ProxmoxViewSet, basename='proxmox')
+router.register('vms', ProxmoxViewSet, basename='proxmox')
 
 urlpatterns = [
     path('', include(router.urls)),
